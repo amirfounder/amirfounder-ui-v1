afac6f67d80310f5697c9d81a536c6a50a251864
Amir Sharapov <amirfounder18@gmail.com>
2021-05-16 02:07:18 -0500

    editted configs for deployment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { MainProvider } from './context/MainContext';
 
 ReactDOM.render(
